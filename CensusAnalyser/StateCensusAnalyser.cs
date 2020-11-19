42ffc2126e3c94334d240312696eab6f800f6daf
asmitamthorat <asmita26296@gmail.com>
2020-11-19 14:30:04 +0530

    ADDED:loadCensus data to get a list

---
+++ CensusAnalyser/StateCensusAnalyser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CensusAnalyser
+{
+    public class StateCensusAnalyser
+    {
+        List<StateCensusDataDTO> StateCensusAnalyserlist = new List<StateCensusDataDTO>();
+        public Dictionary<String, List<StateCensusDataDTO>> Dictionary = new Dictionary<string, List<StateCensusDataDTO>>();
+        public List<StateCensusDataDTO> loadStateCensusData(string path) {
+
+            var file = new System.IO.StreamReader(path);
+            StateCensusAnalyserlist = new CsvHelper.CsvReader(file, System.Globalization.CultureInfo.InvariantCulture)
+                    .GetRecords<StateCensusDataDTO>().ToList();
+
+            Dictionary.Add("StateCensusAnalyzer", StateCensusAnalyserlist);
+            var matchKey = "StateCensusAnalyzer";
+            return Dictionary[matchKey];
+
+        }
+    }
+}
--- CensusAnalyserTest/UnitTest1.cs
+++ CensusAnalyserTest/UnitTest1.cs
@@ -1,18 +1,27 @@
+using CensusAnalyser;
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 
 namespace CensusAnalyserTest
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+        string IndiaStateCodeCensusFilePath = @"C:\\Users\\com\\source\\repos\\CensusAnalyserProject\\CensusAnalyserTest\\utilities\\IndiaStateCode.csv";
+        string IndiaCensusDataWithDelimiter = @"C:\\Users\\com\\source\\repos\\CensusAnalyserProject\\CensusAnalyserTest\\utilities\\DelimiterIndiaStateCensusData.csv";
+        string IndiaCensusDataWithWrongFile = @"C:\\Users\\com\\source\\repos\\CensusAnalyserProject\\CensusAnalyserTest\\utilities\\IndiaStateCode.txt";
+        string IndiaStateCensusWithoutHeader = @"C:\\Users\\com\\source\\repos\\CensusAnalyserProject\\CensusAnalyserTest\\utilities\\WrongHeaderIndiaStateCensusData.csv";
+        string IndiaCensusAnalyserWithWrong_File = @"C:\\Users\\com\\source\\repos\\CensusAnalyserProject\\CensusAnalyserTest\\utilities\\IndiaCensusAnalyser.txt";
+        string IndiaStateCodeWithDelimiter = @"C:\\Users\\com\\source\\repos\\CensusAnalyserProject\\CensusAnalyserTest\\utilities\\IndiaStateCode_withDelimiterIssue.csv";
+        string USCensusData = @"C:\\Users\\com\\source\\repos\\CensusAnalyserProject\\CensusAnalyserTest\\utilities\\USCensusData.csv";
+        string IndiaCensusDataFilePath1 = @"C:\\Users\\com\\source\\repos\\CensusAnalyserProject\\CensusAnalyserTest\\utilities\\IndiaStateCensusData.csv";
 
         [Test]
-        public void Test1()
+        public void voidgivenStateCensusData_ifHasCorrectNumberOfRecord_ShouldReturnTrue()
         {
-            Assert.Pass();
+            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+           List<StateCensusDataDTO> list= stateCensusAnalyser.loadStateCensusData(IndiaCensusDataFilePath1);
+            Assert.AreEqual(29, list.Count);
         }
     }
 }
